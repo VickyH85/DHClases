6b5a4bd18efbb9a87cd00fd9cd3585c60d8b05ad
VickyH85 <victoria.higa@gmail.com>
2019-12-10 20:11:07 -0300

    lo que hicimos durante la clase del 10.12.2019 de ejs

--- TemplateEngine10.12.2019/app.js
+++ TemplateEngine10.12.2019/app.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('el servidor esstá andando'));
+app.listen(3000, () => console.log('el servidor está andando'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
